81af8b56d745c1d0f85aeb76798c337a9e5632d9
masakimasui <m.masui2003@gmail.com>
2022-07-31 17:34:07 +0900

    Cosole上で全て$messageと表示されてしまいます

---
+++ sample.js
@@ -0,0 +1,9 @@
+function showAd(message){
+  console.log('----------');
+  console.log('--- ${message} ---');
+  console.log('----------');
+}
+
+showAd('Header Ad');
+showAd('Ad');
+showAd('Footer Ad');
